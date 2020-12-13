5f93b76a6ce177d3a09f5fb4679b12e57a819bff
David <dlum988@gmail.com>
2020-12-13 12:20:04 -0500

    increased logo size

--- src/assets/jss/material-dashboard-react/components/sidebarStyle.js
+++ src/assets/jss/material-dashboard-react/components/sidebarStyle.js
@@ -101,7 +101,7 @@ const sidebarStyle = theme => ({
     marginRight: "15px"
   },
   img: {
-    width: "35px",
+    width: "225px",
     top: "22px",
     position: "absolute",
     verticalAlign: "middle",
