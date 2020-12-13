809443b11de0cbb90b30061264774badcb10d5ed
David <dlum988@gmail.com>
2020-12-13 12:24:10 -0500

    removed text from logo and removed code that opens link to new page

--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -15,7 +15,7 @@ import routes from "routes.js";
 import styles from "assets/jss/material-dashboard-react/layouts/adminStyle.js";
 
 import bgImage from "assets/img/sidebar-2.jpg";
-import logo from "assets/img/reactlogo.png";
+import logo from "assets/img/logo.png";
 
 let ps;
 
@@ -95,7 +95,6 @@ export default function Admin({ ...rest }) {
     <div className={classes.wrapper}>
       <Sidebar
         routes={routes}
-        logoText={"Creative Tim!"}
         logo={logo}
         image={image}
         handleDrawerToggle={handleDrawerToggle}
